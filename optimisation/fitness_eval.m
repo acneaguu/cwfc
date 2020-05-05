85e833c43fa6c710e70e489d5ea2c7a39962cfa6
jinhanbai <baijinhan12@gmail.com>
2020-05-05 17:23:46 +0200

    OF calculator
    Beginnetje aan deze functie

--- optimisation/fitness_eval.m
+++ optimisation/fitness_eval.m
@@ -82,20 +82,7 @@ if PFresults.success == 1
     total_violations = sum(vio);
     checklimits(PFresults);
     %------------------------------------------------------------------------
-    %COSTS
-    %% Ploss
-    %%branch losses
-    [losses] = get_losses(PFresults);
-    Ploss_branch = sum(real(losses));
-    
-    %%shunt losses (in the busses)
-    Ploss_shunt = sum(PFresults.bus(:,CONSTANTS.VM) .^ 2 .* PFresults.bus(:,CONSTANTS.GS)); %%shunt absorption real
-    Ploss_tot = Ploss_branch + Ploss_shunt;
-    %sum(bus(nzsh, VM) .^ 2 .* bus(nzsh, BS))  %%shunt injection reactive
-    
-    %% Tap switches
-    %kk kinderen 123
-    %% Reactor
+    OF = compute_costs(PFresults);
 else
     %%penalise unsuccesful run
     F = 1e50; %%give chancla to unsuccessful run

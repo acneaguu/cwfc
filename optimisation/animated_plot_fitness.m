b3cdd6507fd6036f22f4f381cc7aab2918305191
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-05-07 13:14:31 +0200

    Update animated_plot_fitness.m

--- optimisation/animated_plot_fitness.m
+++ optimisation/animated_plot_fitness.m
@@ -2,11 +2,15 @@
 %%The struct should contain a variable F which is used to plot the fitness
 %%and a matrix X which is used to plot F as a function of up to 2
 %%optimisation variables. With the input 'vars', the position of the
-%%columns of the two variables is specified
+%%columns of the two variables is specified. If vars is empty, the fitness
+%%is plotted as function of the number of iterations
 function animated_plot_fitness(X,F,vars)
-drawrate = 1/144;
+drawrate = 1/144;   %refreshrate of the plot
+yres = 0.01;        %range of ylim w.r.t. final value of F
+Flim = F(end)*[1-yres,1+yres];
 h = animatedline('Color','red','LineWidth',2);
 
+%%open a new figure
 nfig = get(gcf,'Number');
 if nfig > 1
     figure(nfig+1)
@@ -14,21 +18,22 @@ else
     figure(1)
 end
 
+%%check for the number of input vars
 if nargin>2
     if length(vars) > 1
         x1 = X(:,vars(1));
         x2 = X(:,vars(2));
-        axis([min(x1),max(x1),min(x2),max(x2),0,3*F(end)])
+        axis([min(x1),max(x1),min(x2),max(x2),Flim])
         view([45 45])
         drawtype = 2;
     else
         x1 = X(:,vars(1));
-        axis([0.9*min(x1),1.1*max(x1),0,3*F(end)])
+        axis([0.9*min(x1),1.1*max(x1),Flim])
         drawtype = 1;
     end
 else
     x1 = 1:length(F);
-    ylim(F(end)*[0.99,1.01])
+    ylim(Flim)
     drawtype = 1;
 end 
    

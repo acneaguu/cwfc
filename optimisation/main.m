87e2d16dcda07f6ac533be9dfcc0c3edff53f941
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-05-12 11:30:14 +0200

    different settings

--- optimisation/main.m
+++ optimisation/main.m
@@ -53,9 +53,9 @@ ub = [2.5*ones(Optimisation.Nvars-4,1).' 1.149 1.13 0 20];
 initialise_optimisation_options();  %sets the weights of the different 
                                     %constraints and objectives
 Optimisation.Ncases = 1;            %number of evaluated time instances
-Optimisation.Nruns = 10;             %number of runs per case
+Optimisation.Nruns = 33;             %number of runs per case
 Optimisation.Neval = 1e4;           %max allowed function evaluations
-Optimisation.Populationsize = 200;   %size of the population
+Optimisation.Populationsize = 5;   %size of the population
 Optimisation.algorithm = 4; %1 for ga, 2 for pso, 3 for cdeepso %4 for MVMO_SHM
 
 Optimisation.print = 1;

d3f00a4c74a9d94a092634e51d31e452a5fc9696
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-05-12 14:49:14 +0200

    updated round function
    -the round function is now able to round discrete values to prespecified values

--- optimisation/round_discrete_vars.m
+++ optimisation/round_discrete_vars.m
@@ -1,6 +1,13 @@
 %%This function is used to round the varianles of 'Xin  specified by the
-%%logic vector 'which_vars' to the nearest integers.
-function Xout = round_discrete_vars(Xin,which_vars)
+%%logic vector 'which_vars' to the nearest multiples of the values
+%%specified in 'range_vars'. 'range_vars' must have the length of
+%%'which_vars'. If 'range_vars' is empty, then the variables in
+%%'which_vars' are rounded to the nearest integer
+function Xout = round_discrete_vars(Xin,which_vars,range_vars)
     Xout = Xin;
-    Xout(which_vars) = round(Xin(which_vars));
+    if nargin > 2
+        Xout(which_vars) = range_vars(which_vars) .* floor(Xin(which_vars)./range_vars(which_vars));
+    else 
+        Xout(which_vars) = round(Xin(which_vars));
+    end
 end

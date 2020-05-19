8029e72f7686641c42d7cdb3d65651dfe3b06291
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-05-19 09:35:40 +0200

    removed rchanges

--- optimisation/Initialisation Functions/initialise_results_struct.m
+++ optimisation/Initialisation Functions/initialise_results_struct.m
@@ -5,7 +5,7 @@ global Results Optimisation
 
 Results.Ploss = NaN * zeros(Optimisation.Nruns+1,1);
 Results.tchanges = NaN * zeros(Optimisation.Nruns+1,1);
-Results.rchanges = NaN * zeros(Optimisation.Nruns+1,1);
+Results.Reactors_on = NaN * zeros(Optimisation.Nruns+1,1);
 Results.Qaccuracy = NaN * zeros(Optimisation.Nruns+1,1);
 Results.Fbest = NaN * zeros(Optimisation.Nruns+1,1);
 Results.Xbest = NaN * zeros(Optimisation.Nruns+1,Optimisation.Nvars);
--- optimisation/main.m
+++ optimisation/main.m
@@ -29,7 +29,7 @@ Optimisation.Nvars = Optimisation.Nturbines + Optimisation.Npv + ...
 Optimisation.which_discrete = [14:16];%#ok<NBRAK> %indeces of the discrete variables
 % Optimisation.steps =[0.0168235 0.0168235 1];%steps of the discrete variables
 logic_optvars();                            %generate logic vectors for different var indeces
-initialise_systemdata(system_13);
+initialise_systemdata(system_13_v2);
 
 %Ones describe the bounds of optimisation variables
 %lb = -30% of Pn (5MW), ub = 40% of Pn

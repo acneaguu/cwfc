61ebb1ab7e48cc2502d5e5b853b212112b2dec28
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-05-29 12:30:12 +0200

    results paramsweep nparticles

--- optimisation/main.m
+++ optimisation/main.m
@@ -35,7 +35,7 @@ Optimisation.Populationsize = 1;   %size of the population
 Optimisation.algorithm = 4; %1 for ga, 2 for pso, 3 for cdeepso %4 for MVMO_SHM
 
 Optimisation.print_progress = 1;    %Plots runs in command window
-Optimisation.print_interval = 500; %Interval of printed steps
+Optimisation.print_interval = 10e3; %Interval of printed steps
 Optimisation.print_pfresults = 0;   %Plots powerflow results of optimal solution
 
 %%settings to plot and store the results of the optimisation
@@ -115,9 +115,9 @@ Ncase = 1:length(v);
 % for kkkk = 1:length(ndimmax)
 % parameter.n_random_last = ndimmax(kkkk);
 global parameter proc
-Optimisation.Populationsize = [1 5 10 20 50 100 200];
+Optimisation.Populationsize = [200 50 1 5 10 20 100];
 %     for j = 2:Optimisation.Ncases+1
-      for j = 2:5
+      for j = 2:2
         %%update the casefile
         %%update boundaries lb/ub
         parameter.n_par=Optimisation.Populationsize(j-1);

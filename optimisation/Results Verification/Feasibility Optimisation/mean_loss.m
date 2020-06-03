140e950e69e5c74e82b263217527c5411ce24540
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-06-03 15:19:55 +0200

    Update mean_loss.m

--- optimisation/Results Verification/Feasibility Optimisation/mean_loss.m
+++ optimisation/Results Verification/Feasibility Optimisation/mean_loss.m
@@ -1,7 +1,7 @@
 summ = 0;
 N = 0;
-for i = 2:21
-    if Results(i).Times_converged == 10
+for i = 2:26
+    if Results(i).Times_converged == 5
         summ = summ + Results(i).Ploss_mean;
         N = N + 1;
     end

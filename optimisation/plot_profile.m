b82ad0696e7337155f64ed209fe434cdeca514bd
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-06-03 15:18:13 +0200

    plot park capability + results

--- optimisation/plot_profile.m
+++ optimisation/plot_profile.m
@@ -1,27 +1,39 @@
 close all
 clear all
 
+%%generate the test profile: for each v, 5 Qsetpoints are requested
 Qref.setpoint =  [-0.286; -0.143; 0; 0.143; 0.286]; 
-v = [3.5 4.5 7 15 15 15 7 4.5 3.5 3.5 3.5 4.5 7 4.5 7 15 15 7  4.5 3.5 3.5];
-%v = [7 7 7 7 7 15 15 15 15 15]';
-%v = [3.5 3.5 3.5 3.5 3.5 4.5 4.5 4.5 4.5 4.5]';
+v = [4.5 4.5 4.5 4.5 4.5 5 5 5 5 5 7 7 7 7 7 12 12 12 12 12 15 15 15 15 15]';
 cases(:,1) = v;
-cases(:,2) = [-0.286 -0.286 -0.286 -0.286 -0.143 0 0 0 -0.143 0 0.143 0.143 0.143 0.286 0.286 0.286 0.143 -0.143 -0.143 0.286 0.286];
-Ncase = 1:length(v);
+cases(:,2) =repmat(Qref.setpoint,5,1);
 
-[P_wtg, Q_wtg] = compute_pq_wtg(v);
+%%repeat last entry for legibility
+cases(26,1) = 15;
+cases(26,2) = 0.286;
+
+%%compute the P/Q for the different cases in p.u.
+[P_wtg, Q_wtg] = compute_pq_wtg(cases(:,1));
 P = sum(P_wtg,2)/350;
 Q = sum(Q_wtg,2)/350;
 
+%%plot variables
+axes_fontsize = 15;
+titlesize = 20;
+Ncase = 1:length(cases(:,1));
 
+%%plot
 figure
-title('Test profile optimisation unit')
-yyaxis left
+title('Test profile optimisation unit','FontSize',titlesize)
 hold on
+%left plot is P
+yyaxis left
 plot(Ncase,P,'-b');
-%plot(Ncase,Q,'-g');
-ylabel('P_{max} [p.u.]')
+ylabel('P_{max} [p.u.]','FontSize',axes_fontsize)
+%right plot is Qsetpoint
 yyaxis right
 stairs(Ncase,cases(:,2),'Color','red');
-ylabel('Q_{setpoint} [p.u.]')
-xlabel('Case')
+ylabel('Q_{setpoint} [p.u.]','FontSize',axes_fontsize)
+xlabel('Case','FontSize',axes_fontsize)
+
+ax = gca;
+ax.FontSize = axes_fontsize;

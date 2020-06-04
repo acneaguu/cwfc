34630e61a460b39aafaf5f9bbce28d8230d825a5
jinhanbai <baijinhan12@gmail.com>
2020-06-04 12:27:51 +0200

    jajoh prima commit

--- optimisation/Results Verification/Feasibility Optimisation New Profile/read_results.m
+++ optimisation/Results Verification/Feasibility Optimisation New Profile/read_results.m
@@ -20,37 +20,49 @@ Ploss_withopt = zeros(Ndatapoints,3);
 N = 1;
 for i = 2:length(Data{1,2}.Results)
     if Data{1,2}.Results(i).Times_converged >= 5
-        Ploss_noopt(N,1) = Data{1,2}.Results(i).Ploss_mean;   %%no opt
-        Ploss_withopt(N,1) = Data{1,3}.Results(i).Ploss_mean; %%opt with Q
-        Ploss_withopt(N,2) = Data{1,4}.Results(i).Ploss_mean; %%opt with Q and taps
-        Ploss_withopt(N,3) = Data{1,1}.Results(i).Ploss_mean; %%opt with Q and R
-        N=N+1;
+        Ploss_noopt(i-1,1) = Data{1,2}.Results(i).Ploss_mean;   %%no opt
+        Ploss_withopt(i-1,1) = Data{1,3}.Results(i).Ploss_mean; %%opt with Q
+        Ploss_withopt(i-1,2) = Data{1,4}.Results(i).Ploss_mean; %%opt with Q and taps
+        Ploss_withopt(i-1,3) = Data{1,1}.Results(i).Ploss_mean; %%opt with Q and  
+    else
+         Ploss_noopt(i-1,1) = 0;
+         Ploss_withopt(i-1,1) = 0;
+         Ploss_withopt(i-1,2) = 0;
+         Ploss_withopt(i-1,3) 
     end
 end
 
 
-cases = 1:Ndatapoints;
+whichcase = 1:25;
 darkblue = '#000066';
 blue = '#0000ff';
 lightblue = '#00ffff';
 
 fig = figure(1);
 left_col = [0 0 0];
-right_col = [124/255 252/255 0];
-set(fig,'defaultAxesColorOrder',[left_col;right_col]);
+right_col = [0/255 128/255 0];
+set(fig,'defaultAxesColorOrder',[left_col;left_col]);
 hold on
 xlabel('Case')
 yyaxis left
-plot(cases,Ploss_noopt,'red')           %%no opt
-plot(cases,Ploss_withopt(:,1),'-','Color',blue)   %%opt with Q
+plot(whichcase,Ploss_noopt,'-o','Color','red')           %%no opt
+plot(whichcase,Ploss_withopt(:,1),'-o','Color',blue)   %%opt with Q
 ylabel('Loss [MW]')
-title('No Optimisation vs Optimisation','FontSize',20);
+title('Active power losses: No Optimisation vs Optimisation','FontSize',20);
 reduction_losses(:,1) = 1e3.*(Ploss_noopt-Ploss_withopt(:,1));
 
 yyaxis right
-plot(cases,reduction_losses(:,1),'#7CFC00');
-ylabel('Reduction in losses [kW]');
+plot(whichcase,reduction_losses(:,1),'-o','Color',right_col);
+ylabel('Loss Reduction [kW]');
 legend('Without Optimisation','With Optimisation','Feasibility of Optimisation');
+
+axes_fontsize = 15;
+ax = gca;
+ax.FontSize = axes_fontsize;
+lgd = legend('Without Optimisation','With Optimisation','Feasibility of Optimisation');
+lgd.FontSize = 15;
+lgd.Location = 'northwest';
+
 
 figure(2)
 reduction_losses(:,2) = 1e3*(Ploss_withopt(:,1)-Ploss_withopt(:,2)); % q and taps
@@ -58,19 +70,30 @@ reduction_losses(:,3) = 1e3*(Ploss_withopt(:,1)-Ploss_withopt(:,3)); % q and rea
 
 subplot(121)
 hold on
-plot(cases,Ploss_withopt(:,1),'-','Color',blue)         %%opt with Q
-plot(cases,Ploss_withopt(:,2),'--','Color',darkblue)    %%opt with Q and taps
-plot(cases,Ploss_withopt(:,3),'-.','Color',lightblue)    %%opt with Q and R
+plot(whichcase,Ploss_withopt(:,1),'-o','Color',blue)         %%opt with Q
+plot(whichcase,Ploss_withopt(:,2),'--o','Color',darkblue)    %%opt with Q and taps
+plot(whichcase,Ploss_withopt(:,3),'-.o','Color',lightblue)    %%opt with Q and R
 xlabel('Case')
-ylabel('Active Power Loesoe [MW]')
+ylabel('Loss [MW]')
 legend('Losses with Q','Losses with Q and taps', 'Losses with Q and reactor'); 
+
+ax = gca;
+ax.FontSize = axes_fontsize;
+lgd = legend('Losses with Q','Losses with Q and taps', 'Losses with Q and reactor'); 
+lgd.FontSize = 15;
+lgd.Location = 'northwest';
 
 subplot(122)
 hold on
 %plot(cases,reduction_losses(:,1),'Color',blue)
-plot(cases,reduction_losses(:,2),'--','Color',darkblue)      %reduction due to taps vs only q
-plot(cases,reduction_losses(:,3),'-.','Color',lightblue)     %reduction due to reactor vs only q
+plot(whichcase,reduction_losses(:,2),'--o','Color',darkblue)      %reduction due to taps vs only q
+plot(whichcase,reduction_losses(:,3),'-.o','Color',lightblue)     %reduction due to reactor vs only q
 xlabel('Case')
 ylabel('Reduction in losses [kW]')
 legend('Reduction with taps','Reduction with reactor');
+ax = gca;
+ax.FontSize = axes_fontsize;
+lgd = legend('Reduction with taps','Reduction with reactor'); 
+lgd.FontSize = 15;
+lgd.Location = 'northwest';
         

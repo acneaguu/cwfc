0d3a3b4dd3a3bad5da3bd687e5ea8330cf207c69
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-06-10 12:57:41 +0200

    new param

--- optimisation/Results Verification/MOP Final par/plot_results.m
+++ optimisation/Results Verification/MOP Final par/plot_results.m
@@ -13,6 +13,10 @@ costs_withopt = NaN * ones(1,25);
 Ploss_withopt = NaN * ones(1,25);
 costs_woopt = NaN * ones(1,25);
 Ploss_woopt = NaN * ones(1,25);
+costs_diffpar = NaN * ones(1,25);
+Ploss_diffpar = NaN * ones(1,25);
+costs_onlyP = NaN * ones(1,25);
+Ploss_onlyP = NaN * ones(1,25);
 
 for i = 2:26
     if i ~= 2&&i ~= 3&&i ~= 7
@@ -20,11 +24,19 @@ for i = 2:26
         Ploss_withopt(i-1) = Data{1,1}.Results(i).Ploss_best;
         costs_woopt(i-1) = Data{1,2}.Results(i).total_cost_per_case;
         Ploss_woopt(i-1)  = Data{1,2}.Results(i).Ploss_mean;
+        costs_diffpar(i-1) = Data{1,3}.Results(i).total_cost_per_case;
+        Ploss_diffpar(i-1)  = Data{1,3}.Results(i).Ploss_mean;
+        costs_onlyP(i-1) = Data{1,4}.Results(i).total_cost_per_case;
+        Ploss_onlyP(i-1)  = Data{1,4}.Results(i).Ploss_mean;
     else
        costs_withopt(i-1) = 0;
        Ploss_withopt(i-1) = 0;
        costs_woopt(i-1) = 0;
        Ploss_woopt(i-1) = 0;
+       costs_diffpar(i-1) = 0;
+       Ploss_diffpar(i-1)  = 0;
+       costs_onlyP(i-1) = 0;
+       Ploss_onlyP(i-1)  = 0;
     end
 end
 
@@ -33,6 +45,8 @@ vcase = 1:25;
 green1 = '#04cc82';
 red1 = '#cc0000';
 darkblue = '#000066';
+orange = '#e59400';
+cyan = '#00FFFF';
 axes_fontsize = 15;
 titlesize = 20;
 
@@ -46,10 +60,13 @@ ax.FontSize = axes_fontsize;
 hold on
 plot(vcase,costs_withopt,'Color',green1)
 plot(vcase,costs_woopt,'Color',red1)
+plot(vcase,costs_diffpar,'Color',orange)
+% plot(vcase,costs_onlyP,'Color',darkblue)
 title('Costs With and Without Optimisation','FontSize',titlesize)
 xlabel('case')
 ylabel('cost [€]')
-lgd = legend('Costs with optimisation','Costs without optimisation');
+lgd = legend('Costs with optimisation','Costs without optimisation',...
+    'Costs with different parameters','Costs with only P_{loss} optimisation');
 lgd.Location = 'northwest';
 lgd.FontSize = axes_fontsize;
 
@@ -61,9 +78,12 @@ hold on
 title('P_{loss} With and Without Optimisation','FontSize',titlesize)
 plot(vcase,Ploss_withopt,'Color',green1)
 plot(vcase,Ploss_woopt,'Color',red1)
+plot(vcase,Ploss_diffpar,'Color',orange)
+plot(vcase,Ploss_onlyP,'Color',darkblue)
 xlabel('case')
 ylabel('P_{loss} [MW]')
-lgd = legend('P_{loss} with optimisation','P_{loss} without optimisation');
+lgd = legend('P_{loss} with optimisation','P_{loss} without optimisation',...
+    'P_{loss} with different parameters','P_{loss} with only P_{loss} optimisation');
 lgd.Location = 'northwest';
 lgd.FontSize = axes_fontsize;
 
@@ -72,13 +92,16 @@ days = 0:365;
 factor = 24/(25*0.25); %25 cases to day
 
 total_costs_withopt = sum(costs_withopt);
+total_costs_diffpar = sum(costs_diffpar);
 total_costs_woopt = sum(costs_woopt);
 
 total_costs_withopt_cum = zeros(1,length(days));
+total_costs_diffpar_cum = zeros(1,length(days));
 total_costs_woopt_cum = zeros(1,length(days));
  
 for i = 2:length(days)
     total_costs_withopt_cum(i) = total_costs_withopt_cum(i-1) + total_costs_withopt * factor;
+    total_costs_diffpar_cum(i) = total_costs_diffpar_cum(i-1) + total_costs_diffpar * factor;
     total_costs_woopt_cum(i) = total_costs_woopt_cum(i-1) + total_costs_woopt * factor;
 end
 
@@ -93,10 +116,12 @@ xlabel('days')
 yyaxis left
 plot(days,total_costs_withopt_cum,'-','Color',green1)
 plot(days,total_costs_woopt_cum,'-','Color',red1)
+plot(days,total_costs_diffpar_cum,'Color',orange)
+
 ylabel('cumulative cost [€]')
 
 yyaxis right
-plot(days,total_savings_cum,'-','Color',darkblue)
+% plot(days,total_savings_cum,'-','Color',darkblue)
 ylabel('cumulative savings [€]')
 
 ax = gca;

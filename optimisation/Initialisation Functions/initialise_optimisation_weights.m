b8bc58c5b708da6e848b2e3a5fd5d59f872991ba
jinhanbai <baijinhan12@gmail.com>
2020-06-11 14:36:50 +0200

    1

--- optimisation/Initialisation Functions/initialise_optimisation_weights.m
+++ optimisation/Initialisation Functions/initialise_optimisation_weights.m
@@ -9,15 +9,15 @@ Optimisation.p2 = 2;                %Penalty for Qref violation
 Optimisation.p3 = 1;                %Penalty for power violation
 
 %Weights of the OF
-Optimisation.w1 = 0.5;              %Weight of Ploss
-Optimisation.w2 = 0.4;                %Weight of transformer switches
-Optimisation.w3 = 0.05;             %Weight of reactor switching
-Optimisation.w4 = 0.05;             %Weight of the extremeness of Qstrings
+% Optimisation.w1 = 0.5;              %Weight of Ploss
+% Optimisation.w2 = 0.4;                %Weight of transformer switches
+% Optimisation.w3 = 0.05;             %Weight of reactor switching
+% Optimisation.w4 = 0.05;             %Weight of the extremeness of Qstrings
 
-% Optimisation.w1 = 0;              %Weight of Ploss
-% Optimisation.w2 = 0;                %Weight of transformer switches
-% Optimisation.w3 = 0;             %Weight of reactor switching
-% Optimisation.w4 = 0;             %Weight of the extremeness of Qstrings
+Optimisation.w1 = 0;              %Weight of Ploss
+Optimisation.w2 = 0;                %Weight of transformer switches
+Optimisation.w3 = 0;             %Weight of reactor switching
+Optimisation.w4 = 0;             %Weight of the extremeness of Qstrings
 
 %Cost of objectives
 Optimisation.timeinterval = 0.25;   %time interval per case; used for 

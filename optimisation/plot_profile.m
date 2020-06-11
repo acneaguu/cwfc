2e38b80e6f21a9109ecf89ca7417dfa7fc93c15f
acneaguu <45164974+acneaguu@users.noreply.github.com>
2020-06-11 11:52:07 +0200

    added testprofile

--- optimisation/plot_profile.m
+++ optimisation/plot_profile.m
@@ -4,17 +4,25 @@ clear all
 %%generate the test profile: for each v, 5 Qsetpoints are requested
 Qref.setpoint =  [-0.286; -0.143; 0; 0.143; 0.286]; 
 v = [4.5 4.5 4.5 4.5 4.5 5 5 5 5 5 7 7 7 7 7 12 12 12 12 12 15 15 15 15 15]';
+irradiance = [0 0 0 0 0 340 340 340 340 340 680 680 680 680 680 ...
+        510 510 510 510 510 170 170 170 170 170];
 cases(:,1) = v;
 cases(:,2) =repmat(Qref.setpoint,5,1);
+cases(:,3) = irradiance;
 
 %%repeat last entry for legibility
 cases(26,1) = 15;
 cases(26,2) = 0.286;
+cases(26,3) = 170;
 
 %%compute the P/Q for the different cases in p.u.
 [P_wtg, Q_wtg] = compute_pq_wtg(cases(:,1));
 P = sum(P_wtg,2)/350;
 Q = sum(Q_wtg,2)/350;
+
+[Ppvg,Qpvg] = compute_pq_pvg(cases(:,3),4);
+Ppvg = sum(Ppvg,2)/350;
+Qpvg = sum(Qpvg,2)/350;
 
 %%plot variables
 axes_fontsize = 15;
@@ -22,12 +30,14 @@ titlesize = 20;
 Ncase = 1:length(cases(:,1));
 
 %%plot
-figure
+fig = figure(1);
+set(fig,'defaultAxesColorOrder',[0, 0, 0;0, 0, 0]);
 title('Test Profile Optimisation Unit','FontSize',titlesize)
 hold on
 %left plot is P
 yyaxis left
-plot(Ncase,P,'-b');
+plot(Ncase,P,'-b','LineWidth',1.5);
+plot(Ncase,Ppvg,'-','Color','#ff9d3b','LineWidth',1.5);
 ylabel('P_{max} [p.u.]','FontSize',axes_fontsize)
 %right plot is Qsetpoint
 yyaxis right
